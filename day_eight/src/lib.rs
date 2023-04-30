720fa2adb39120bd320063aa65bc8948ed94d888
ljkeller <kellerjlucas@gmail.com>
2023-04-30 13:33:50 -0500

    Day 8 part 2

--- day_eight/src/lib.rs
+++ day_eight/src/lib.rs
@@ -32,6 +32,49 @@ pub fn visible_one(trees_fp: &str) -> usize {
     return visible_trees;
 }
 
+pub fn visible_two(trees_fp: &str) -> u64 {
+    let mut max_view_score = 0;
+
+    let mut forest: Vec<Vec<u8>> = Vec::new();
+    if let Ok(trees_string) = std::fs::read_to_string(trees_fp) {
+        trees_string.lines().for_each(|tree_row| {
+            forest.push(
+                tree_row
+                    .chars()
+                    .map(|c| c.to_digit(10).expect("Expect digit") as u8)
+                    .collect(),
+            )
+        });
+
+        for (row_idx, tree_row) in forest.iter().enumerate() {
+            for (col_idx, tree) in tree_row.iter().enumerate() {
+                max_view_score =
+                    std::cmp::max(max_view_score, tree_score(&forest, row_idx, col_idx, &tree));
+            }
+        }
+    } else {
+        println!(
+            "Couldnt find file: {} at path {}",
+            trees_fp,
+            std::env::current_dir().unwrap().display()
+        );
+    }
+
+    return max_view_score;
+}
+
+fn tree_score(
+    forest: &Vec<Vec<u8>>,
+    target_row: usize,
+    target_col: usize,
+    target_tree: &u8,
+) -> u64 {
+    return vis_to_left(forest, target_row, target_col, target_tree)
+        * vis_to_right(forest, target_row, target_col, target_tree)
+        * vis_to_top(target_row, forest, target_col, target_tree)
+        * vis_to_bottom(target_row, forest, target_col, target_tree);
+}
+
 // trees on perimeter: visible
 // trees not in perimeter: visible only if visible from at least one direction
 fn tree_visible(
@@ -112,6 +155,70 @@ fn vis_from_left(
     vis_from_left
 }
 
-pub fn visible_two(trees_fp: &str) -> usize {
-    return 0;
+fn vis_to_bottom(
+    target_row: usize,
+    forest: &Vec<Vec<u8>>,
+    target_col: usize,
+    target_tree: &u8,
+) -> u64 {
+    let mut visible = 0;
+    for row in target_row + 1..forest.len() {
+        let cur_tree = forest[row][target_col];
+        visible += 1;
+        if &cur_tree >= target_tree {
+            break;
+        }
+    }
+    visible
+}
+
+fn vis_to_top(
+    target_row: usize,
+    forest: &Vec<Vec<u8>>,
+    target_col: usize,
+    target_tree: &u8,
+) -> u64 {
+    let mut visible = 0;
+    // order matters, as we extend from current tree to top of forest
+    for row in (0..target_row).rev() {
+        let cur_tree = forest[row][target_col];
+        visible += 1;
+        if &cur_tree >= target_tree {
+            break;
+        }
+    }
+    visible
+}
+
+fn vis_to_right(
+    forest: &Vec<Vec<u8>>,
+    target_row: usize,
+    target_col: usize,
+    target_tree: &u8,
+) -> u64 {
+    let mut visible = 0;
+    for cur_tree in forest[target_row][target_col + 1..].iter() {
+        visible += 1;
+        if cur_tree >= target_tree {
+            break;
+        }
+    }
+    visible
+}
+
+fn vis_to_left(
+    forest: &Vec<Vec<u8>>,
+    target_row: usize,
+    target_col: usize,
+    target_tree: &u8,
+) -> u64 {
+    let mut visible = 0;
+    // order matters, as we extend from current tree to left side of forest
+    for cur_tree in forest[target_row][..target_col].iter().rev() {
+        visible += 1;
+        if cur_tree >= target_tree {
+            break;
+        }
+    }
+    visible
 }
